95ade071c5d611cfbc3ed6571ddfcc28b33042e1
HomeWork <pirliciadrian@gmail.com>
2023-12-09 20:19:44 +0200

    Added infrastructure projects.

---
+++ src/Infrastructure/DAL/DoctorFactory.DAL/ServiceRegistrator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DoctorFactory.DAL;
+public static class ServiceRegistrator
+{
+    /// <summary> Extension method to configure and add infrastructure services to the IServiceCollection. </summary>
+    /// <param name="services">The IServiceCollection to which services should be added.</param>
+    /// <returns>The updated IServiceCollection.</returns>
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
