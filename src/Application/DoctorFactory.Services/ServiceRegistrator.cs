8f18c11dbd16270c041daea798075a6ea38cbc4d
HomeWork <pirliciadrian@gmail.com>
2023-12-09 19:51:10 +0200

    Added application layer projects.

---
+++ src/Application/DoctorFactory.Services/ServiceRegistrator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DoctorFactory.Services;
+public static class ServiceRegistrator
+{
+    /// <summary> Extension method to configure and add Doctor Factory related services to the IServiceCollection. </summary>
+    /// <param name="services">The IServiceCollection to which services should be added.</param>
+    /// <returns>The updated IServiceCollection.</returns>
+    public static IServiceCollection AddDoctorFactoryServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
